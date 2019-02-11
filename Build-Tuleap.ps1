cc77328bd0fa84c51618c1b21b46ffa822ad0b72
Oussama DELASSI <1280129+delassio@users.noreply.github.com>
2019-02-11 18:25:07 +0100

    First Commit
    From local machine (build success)
    2019/02/11 16:59:23 packer.exe: 2019/02/11 16:59:23 [INFO] Packer version: 1.3.4
    2019/02/11 16:59:23 packer.exe: 2019/02/11 16:59:23 Packer Target OS/Arch: windows amd64
    ==> Builds finished. The artifacts of successful builds are:
    2019/02/11 17:20:33 ui: --> vmware-iso: VM files in directory: output-vmware-iso

---
+++ Build-Tuleap.ps1
@@ -0,0 +1,100 @@
+Function Connect-Px($SERVER) {
+    if($SERVER.Length -eq 0)
+    {
+        Write-Host "No input detected"
+        return $False
+    } 
+    elseif ($SERVER.Length -gt 0 )
+    { 
+                Write-Host "Connecting to $Server ..."
+                $HOSTIP, $PORT= $SERVER.split(":")
+                if ([string]::IsNullOrEmpty($PORT)) {$PORT="80"}
+                if (Test-NetConnection -ComputerName $HOSTIP -Port $PORT -InformationLevel Quiet)
+                            {
+                                Write-Host "Successfully connected into Enterprise proxy $SERVER... :)"
+                                return $True
+                            }
+                            else 
+                            {
+                                Write-Host "Failed to connect into $SERVER :("
+                                return $False   
+                            }
+    }
+}
+
+
+Function Test-Px {    
+do
+    {
+        if($env:http_proxy.Length -gt 0)
+        {        
+            $env:px_server= $env:http_proxy.trimstart("http://")
+            Write-Host "Default PROXY ($env:px_server)"
+        }
+else    {
+            Write-Host "No SYSTEM PROXY SETTING detected"
+            $env:px_server= Read-Host -Prompt "Enter Enterprise Proxy <IP:port>, <hostname:port> ?"
+            Clear-Host        
+        }
+                                                              
+    } while (-not (Connect-Px($env:px_server)))
+    return $True
+}
+
+Function Get-Rootpw {    
+    do
+        {
+            if($env:rootpw.Length -gt 0)
+            {        
+                Write-Host "Default root password ($env:rootpw)"
+            }
+    else    {
+                Write-Host "error No root password is set, set it as rootpw in your environment."
+                $env:rootpw= Read-Host -Prompt "Enter root password ?"
+                Write-Output $env:rootpw | Out-File rootpw
+
+                Clear-Host        
+            }
+                                                                  
+        } while (([string]::IsNullOrEmpty($env:rootpw)))
+    }
+
+Function Start-Px ($CHECK) {
+    if ($CHECK){
+    Clear-Host
+    Write-Host "Starting Px Server $env:px_server"    
+    $IF=$(Get-DnsClient -ConnectionSpecificSuffix $env:USERDNSDOMAIN).InterfaceIndex
+
+    $env:px_listen=$(Get-NetIPAddress -InterfaceIndex $IF -AddressFamily IPv4).IPAddress
+
+    $env:http_proxy='http://'+$env:px_listen+':3128'
+
+    Write-Host 'Enterprise Proxy'$env:px_server
+
+    Write-Host 'Px Proxy'$env:http_proxy
+
+    $env:px_username=$env:USERDOMAIN+'\'+$env:USERNAME
+
+    Write-Host 'Px username'$env:px_username
+    Get-Rootpw
+    Start-Process -Verb open -WorkingDirectory px cmd.exe -ArgumentList "/c", "px.exe", "--server=$env:px_server", "--listen=$env:px_listen", "--user=$env:px_username", "--foreground", "--debug"}
+    else {Write-Host "Failed to start Px server $env:px_server :("}
+}
+
+Function Stop-Px {
+    Start-Process -Verb open -WorkingDirectory px px.exe -ArgumentList "--quit"
+}
+
+Function Build-Tuleap {
+    Write-Host 'Start Building Tuleap VM'
+    Write-Host 'Root Password'$env:rootpw 
+    $env:PACKER_LOG=1
+    $env:PACKER_LOG_PATH="packerlog.txt"     
+    packer build packerConfig.json
+}
+
+
+
+Start-Px(Test-Px)
+Build-Tuleap
+Stop-Px
---
+++ README.md
@@ -0,0 +1,32 @@
+TULEAP VM Builder
+===============================
+
+Welcome to the packer build for tuleap vm.  
+This will build a Tuleap ALM and configure Active Directory for users login.  Follow the download instructions to download the software to install and follow the build instructions to build the machine.  You will need around 3 Gb of space free to store the downloads and run the build.
+For getting accees to enterprise internet you can use px server that support  ntlm auth. 
+
+There are several directories which are used in the build
+
+1. put_files_here - place ALL downloaded software here (centos iso)
+2. upload - ldap customization files (json file)
+3. scripts - This directory has all the install scripts for the tuleap setup and ldap plugin avtivation.
+4. px - px server for windows ntlm proxy    
+
+Mandatory Downloads
+-------------------
+**These two downloads are mandatory**.  If they are not here, the build will not start.
+
+* CentOS-7-x86_64-Minimal-1810.iso (needs to be this exactly md5 checked)
+
+* Px programm px.exe for ntlm proxy
+
+Build Instructions
+------------------
+run Tuleap.ink shortcut 
+
+or from cmd prompt run>  pwsh.exe -ExecutionPolicy Bypass -command ".\Build-Tuleap.ps1"
+
+
+Build Structure
+--------------------
+ to be done 
