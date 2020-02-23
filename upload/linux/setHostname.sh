45e635988ca1af53d4d6c7a7326e30fde28e492a
Oussama DELASSI <1280129+delassio@users.noreply.github.com>
2020-02-23 22:57:43 +0100

    fix setHostame script

--- upload/linux/setHostname.sh
+++ upload/linux/setHostname.sh
@@ -7,24 +7,22 @@ fi
 if [ -z "$domainname" ] 
 then
 fqdn="$hostname.localdomain"
-hostnamectl set-hostname $fqdn
+hostnamectl set-hostname $fqdn --static
 echo "INSTALLER: FQDN: "$fqdn" will be used."
 # /etc/init.d/network restart
 elif [ -n "$domainname" ]
 then
 fqdn="$hostname.$domainname"
-hostnamectl set-hostname $fqdn
+hostnamectl set-hostname $fqdn --static
 echo "INSTALLER: FQDN: "$fqdn" will be used."      
 fi
 
-echo "HOSTNAME="$(hostname -A) > /etc/sysconfig/network
-echo $(hostname -I) $(hostname -A) $(hostname -s) > /etc/hosts
-
-
-echo "INSTALLER: /etc/hosts: $(cat /etc/hosts)" 
 
 
 echo "INSTALLER: /etc/hostname: $(cat /etc/hostname)"
 
 
-echo "INSTALLER: /etc/sysconfig/network: $(cat /etc/sysconfig/network)"  
+echo $(hostname -I) $(hostname -A) $(hostname -s) > /etc/hosts
+
+
+echo "INSTALLER: /etc/hosts: $(cat /etc/hosts)"
